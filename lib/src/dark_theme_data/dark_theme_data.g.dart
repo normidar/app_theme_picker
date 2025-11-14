8a4b0b97620496b37c68afb862cbb37480f5b1c2
normidar <normidar7@gmail.com>
2025-11-14 21:37:49 +0900

    Update pubspec.yaml dependencies to latest versions and refactor generated theme data provider files for improved structure and clarity. Bump versions for flutter_riverpod, riverpod_annotation, build_runner, riverpod_generator, and riverpod_lint.

--- lib/src/dark_theme_data/dark_theme_data.g.dart
+++ lib/src/dark_theme_data/dark_theme_data.g.dart
@@ -6,171 +6,93 @@ part of 'dark_theme_data.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$darkThemeDataHash() => r'49ea6d5e543f189ff632fbf3115299b4bb1c8cc8';
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
 
-/// Copied from Dart SDK
-class _SystemHash {
-  _SystemHash._();
-
-  static int combine(int hash, int value) {
-    // ignore: parameter_assignments
-    hash = 0x1fffffff & (hash + value);
-    // ignore: parameter_assignments
-    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
-    return hash ^ (hash >> 6);
-  }
-
-  static int finish(int hash) {
-    // ignore: parameter_assignments
-    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
-    // ignore: parameter_assignments
-    hash = hash ^ (hash >> 11);
-    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
-  }
-}
-
-abstract class _$DarkThemeData
-    extends BuildlessAutoDisposeAsyncNotifier<ThemeData> {
-  late final String? fontFamily;
-
-  FutureOr<ThemeData> build([
-    String? fontFamily,
-  ]);
-}
-
-/// See also [DarkThemeData].
 @ProviderFor(DarkThemeData)
-const darkThemeDataProvider = DarkThemeDataFamily();
+const darkThemeDataProvider = DarkThemeDataFamily._();
 
-/// See also [DarkThemeData].
-class DarkThemeDataFamily extends Family<AsyncValue<ThemeData>> {
-  /// See also [DarkThemeData].
-  const DarkThemeDataFamily();
-
-  /// See also [DarkThemeData].
-  DarkThemeDataProvider call([
-    String? fontFamily,
-  ]) {
-    return DarkThemeDataProvider(
-      fontFamily,
-    );
-  }
-
-  @override
-  DarkThemeDataProvider getProviderOverride(
-    covariant DarkThemeDataProvider provider,
-  ) {
-    return call(
-      provider.fontFamily,
-    );
-  }
-
-  static const Iterable<ProviderOrFamily>? _dependencies = null;
-
-  @override
-  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
-
-  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
-
-  @override
-  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
-      _allTransitiveDependencies;
-
-  @override
-  String? get name => r'darkThemeDataProvider';
-}
-
-/// See also [DarkThemeData].
-class DarkThemeDataProvider
-    extends AutoDisposeAsyncNotifierProviderImpl<DarkThemeData, ThemeData> {
-  /// See also [DarkThemeData].
-  DarkThemeDataProvider([
-    String? fontFamily,
-  ]) : this._internal(
-          () => DarkThemeData()..fontFamily = fontFamily,
-          from: darkThemeDataProvider,
+final class DarkThemeDataProvider
+    extends $AsyncNotifierProvider<DarkThemeData, ThemeData> {
+  const DarkThemeDataProvider._(
+      {required DarkThemeDataFamily super.from,
+      required String? super.argument})
+      : super(
+          retry: null,
           name: r'darkThemeDataProvider',
-          debugGetCreateSourceHash:
-              const bool.fromEnvironment('dart.vm.product')
-                  ? null
-                  : _$darkThemeDataHash,
-          dependencies: DarkThemeDataFamily._dependencies,
-          allTransitiveDependencies:
-              DarkThemeDataFamily._allTransitiveDependencies,
-          fontFamily: fontFamily,
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
         );
 
-  DarkThemeDataProvider._internal(
-    super._createNotifier, {
-    required super.name,
-    required super.dependencies,
-    required super.allTransitiveDependencies,
-    required super.debugGetCreateSourceHash,
-    required super.from,
-    required this.fontFamily,
-  }) : super.internal();
-
-  final String? fontFamily;
+  @override
+  String debugGetCreateSourceHash() => _$darkThemeDataHash();
 
   @override
-  FutureOr<ThemeData> runNotifierBuild(
-    covariant DarkThemeData notifier,
-  ) {
-    return notifier.build(
-      fontFamily,
-    );
+  String toString() {
+    return r'darkThemeDataProvider'
+        ''
+        '($argument)';
   }
 
+  @$internal
   @override
-  Override overrideWith(DarkThemeData Function() create) {
-    return ProviderOverride(
-      origin: this,
-      override: DarkThemeDataProvider._internal(
-        () => create()..fontFamily = fontFamily,
-        from: from,
-        name: null,
-        dependencies: null,
-        allTransitiveDependencies: null,
-        debugGetCreateSourceHash: null,
-        fontFamily: fontFamily,
-      ),
-    );
-  }
-
-  @override
-  AutoDisposeAsyncNotifierProviderElement<DarkThemeData, ThemeData>
-      createElement() {
-    return _DarkThemeDataProviderElement(this);
-  }
+  DarkThemeData create() => DarkThemeData();
 
   @override
   bool operator ==(Object other) {
-    return other is DarkThemeDataProvider && other.fontFamily == fontFamily;
+    return other is DarkThemeDataProvider && other.argument == argument;
   }
 
   @override
   int get hashCode {
-    var hash = _SystemHash.combine(0, runtimeType.hashCode);
-    hash = _SystemHash.combine(hash, fontFamily.hashCode);
-
-    return _SystemHash.finish(hash);
+    return argument.hashCode;
   }
 }
 
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-mixin DarkThemeDataRef on AutoDisposeAsyncNotifierProviderRef<ThemeData> {
-  /// The parameter `fontFamily` of this provider.
-  String? get fontFamily;
-}
+String _$darkThemeDataHash() => r'49ea6d5e543f189ff632fbf3115299b4bb1c8cc8';
 
-class _DarkThemeDataProviderElement
-    extends AutoDisposeAsyncNotifierProviderElement<DarkThemeData, ThemeData>
-    with DarkThemeDataRef {
-  _DarkThemeDataProviderElement(super.provider);
+final class DarkThemeDataFamily extends $Family
+    with
+        $ClassFamilyOverride<DarkThemeData, AsyncValue<ThemeData>, ThemeData,
+            FutureOr<ThemeData>, String?> {
+  const DarkThemeDataFamily._()
+      : super(
+          retry: null,
+          name: r'darkThemeDataProvider',
+          dependencies: null,
+          $allTransitiveDependencies: null,
+          isAutoDispose: true,
+        );
+
+  DarkThemeDataProvider call([
+    String? fontFamily,
+  ]) =>
+      DarkThemeDataProvider._(argument: fontFamily, from: this);
 
   @override
-  String? get fontFamily => (origin as DarkThemeDataProvider).fontFamily;
+  String toString() => r'darkThemeDataProvider';
 }
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
+
+abstract class _$DarkThemeData extends $AsyncNotifier<ThemeData> {
+  late final _$args = ref.$arg as String?;
+  String? get fontFamily => _$args;
+
+  FutureOr<ThemeData> build([
+    String? fontFamily,
+  ]);
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build(
+      _$args,
+    );
+    final ref = this.ref as $Ref<AsyncValue<ThemeData>, ThemeData>;
+    final element = ref.element as $ClassProviderElement<
+        AnyNotifier<AsyncValue<ThemeData>, ThemeData>,
+        AsyncValue<ThemeData>,
+        Object?,
+        Object?>;
+    element.handleValue(ref, created);
+  }
+}
--- lib/src/theme_mode_pod/theme_mode_pod.g.dart
+++ lib/src/theme_mode_pod/theme_mode_pod.g.dart
@@ -6,20 +6,47 @@ part of 'theme_mode_pod.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(ThemeModePod)
+const themeModePodProvider = ThemeModePodProvider._();
+
+final class ThemeModePodProvider
+    extends $AsyncNotifierProvider<ThemeModePod, ThemeMode> {
+  const ThemeModePodProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'themeModePodProvider',
+          isAutoDispose: false,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$themeModePodHash();
+
+  @$internal
+  @override
+  ThemeModePod create() => ThemeModePod();
+}
+
 String _$themeModePodHash() => r'f650944efbe41af12037ea47df5f3f26d3f40e36';
 
-/// See also [ThemeModePod].
-@ProviderFor(ThemeModePod)
-final themeModePodProvider =
-    AsyncNotifierProvider<ThemeModePod, ThemeMode>.internal(
-  ThemeModePod.new,
-  name: r'themeModePodProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$themeModePodHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-typedef _$ThemeModePod = AsyncNotifier<ThemeMode>;
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
+abstract class _$ThemeModePod extends $AsyncNotifier<ThemeMode> {
+  FutureOr<ThemeMode> build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build();
+    final ref = this.ref as $Ref<AsyncValue<ThemeMode>, ThemeMode>;
+    final element = ref.element as $ClassProviderElement<
+        AnyNotifier<AsyncValue<ThemeMode>, ThemeMode>,
+        AsyncValue<ThemeMode>,
+        Object?,
+        Object?>;
+    element.handleValue(ref, created);
+  }
+}
